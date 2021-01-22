06aa96d06f5857f5a6bcef4ea86224d1f2915dcb
julieta96 <julietaextra14@gmail.com>
2021-01-22 19:22:29 -0300

    se agrego validacion para la seccion eventos relacionados

---
+++ js/eventos-relacionados.js
@@ -0,0 +1,125 @@
+
+/*function cambioEvento1(){
+
+
+   
+
+		   let cambioImagen = document.querySelector(".e-img");
+		    cambioImagen.src='img/eventos/2.jpg';
+
+			/*let eventoClickeado = document.querySelector(".evento-r"+i);*/
+			/*let cambioClase = document.querySelector("evento-r2");
+			cambioClase.className = cambioClase[i].className.replace(".evento-r"+i, ".evento-r"+(i+1));*/
+
+			//let cambioTitulo = document.querySelector(".titulo");
+			//cambioTitulo.innerHTML="KARAOKE";
+			//let cambioFechaHora= document.querySelector(".cambio-hora");
+			//cambioFechaHora.innerHTML="02/09/2021 23hs";
+			//let cambioOrganizador= document.querySelector(".cambio-organizador");
+			//cambioOrganizador.innerHTML="Organizador2";
+//}
+
+//function cambioEvento2(){
+
+
+   
+
+		   //let cambioImagen = document.querySelector(".e-img");
+		    //cambioImagen.src='img/eventos/3.jpg';
+
+			/*let eventoClickeado = document.querySelector(".evento-r"+i);*/
+			/*let cambioClase = document.querySelector("evento-r2");
+			cambioClase.className = cambioClase[i].className.replace(".evento-r"+i, ".evento-r"+(i+1));*/
+
+			/*let cambioTitulo = document.querySelector(".titulo");
+			cambioTitulo.innerHTML="CUMPLEAÑOS";
+			let cambioFechaHora= document.querySelector(".cambio-hora");
+			cambioFechaHora.innerHTML="10/07/2021 21hs";
+			let cambioOrganizador= document.querySelector(".cambio-organizador");
+			cambioOrganizador.innerHTML="Organizador3";
+
+
+	
+	
+}*/
+
+//function cambioEvento3(){
+
+
+   
+
+		   //let cambioImagen = document.querySelector(".e-img");
+		   // cambioImagen.src='img/eventos/4.jpg';
+
+			/*let eventoClickeado = document.querySelector(".evento-r"+i);*/
+			/*let cambioClase = document.querySelector("evento-r2");
+			cambioClase.className = cambioClase[i].className.replace(".evento-r"+i, ".evento-r"+(i+1));*/
+
+			/*let cambioTitulo = document.querySelector(".titulo");
+			cambioTitulo.innerHTML="CASAMIENTOS";
+			let cambioFechaHora= document.querySelector(".cambio-hora");
+			cambioFechaHora.innerHTML="20/09/2021 22hs";
+			let cambioOrganizador= document.querySelector(".cambio-organizador");
+			cambioOrganizador.innerHTML="Organizador4";*/
+
+
+	
+	
+//}
+
+/*function click1(){
+
+	let evento1 = document.querySelector(".eventos-r2");
+	evento1=addEventListener("click" , cambioEvento1);
+
+}
+
+function click2(){
+
+	let evento2 = document.querySelector(".eventos-r3");
+	evento2=addEventListener("click" , cambioEvento2);
+	
+}
+
+function click3(){
+
+	let evento3 = document.querySelector(".eventos-r4");
+	evento3=addEventListener("click" , cambioEvento3);
+	
+}*/
+
+/*window.addEventListener("load",*/ 
+
+let arrowL = document.querySelector("#f-izq");
+let arrowR = document.querySelector("#f-der");
+
+//arrowR.addEventListener("click" ,cambiarImg);
+arrowL.addEventListener("click" ,cambiarImgL);
+
+/*function cambiarImgR(){
+
+	let cambioImagen1 = document.querySelector(".eventos-r5");
+	cambioImagen1.src='img/eventos/5.jpg';
+
+	let cambioImagen1 = document.querySelector(".eventos-r6");
+	cambioImagen1.src='img/eventos/6.jpg';
+
+	let cambioImagen1 = document.querySelector(".eventos-r1");
+	cambioImagen1.src='img/eventos/1.jpg';
+
+}*/
+
+function cambiarImgL(){
+
+	let cambioImagen2 = document.querySelector(".eventos-r2");
+	cambioImagen2.src='img/eventos/3.jpg';
+
+	let cambioImagen3 = document.querySelector(".eventos-r3");
+	cambioImagen3.src='img/eventos/4.jpg';
+
+	let cambioImagen4 = document.querySelector(".eventos-r4");
+	cambioImagen4.src='img/eventos/5.jpg';
+	cambioImagen4.style.display="block";
+
+}
+
