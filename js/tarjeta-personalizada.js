8f4f0acc6dfd7c8e6772195990a469965c92f516
julieta96 <julietaextra14@gmail.com>
2021-01-27 16:36:30 -0300

    se agrego validacion de la tarjeta personalizada

---
+++ js/tarjeta-personalizada.js
@@ -0,0 +1,19 @@
+$(document).on("keyup", "#formulario input[name='titulo-e'], #formulario input[name='fecha-hora']", function(event){
+     let titulo=''
+     let fechaHora=''
+
+    if(event.target.name=="titulo-e")
+    {
+   
+        titulo = this.value   
+       $("#titulo-e").text(titulo)
+
+    }else if(event.target.name=="fecha-hora")
+    {
+       fechaHora = this.value
+       $("#fecha-hora").text(fechaHora)
+    }
+    
+    
+
+})
