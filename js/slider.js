805c3dc42de1b58a5f590a6c35e3853cb99844a8
julieta96 <julietaextra14@gmail.com>
2021-01-31 15:19:50 -0300

    se modifico la validacion

--- js/slider.js
+++ js/slider.js
@@ -1,87 +1,39 @@
-var n=-1;
-var slider;
-var bRight;
-var bLeft;
-
-function slider() {
-    bRight=document.getElementById("f-der");
-    bLeft=document.getElementById("f-izq");
-	bRight.addEventListener("click" , right);
-	bLeft.addEventListener("click" , left);
-}
-
-function mostrarImagenes(n){
-
-	for (var i =0; i<slider.length; i++) {
-
-		slider=document.getElementsByClassName("eventos");
-
-		slider[i].style.display="none";
-	}
-  
-
-  slider[n].style.display="block";
+$(document).ready(function(){
+    
 
 
-}
-
-
-
-function right(){
-
-	/*alert("que onda");*/
-
-	slider=document.getElementsByClassName("eventos");
-
-	/*alert(slider.length);*/
-
-	/*alert(n);*/
-
-	if(n<=slider.length-1){
+    $(".dot1").on('click',function(){
        
-		n++;
+          $(this).siblings().removeClass("active");
+          $(this).addClass("active");
+          $(".cambiarImg").attr('src',"img/slider/e1.jpg"); 
+     })
 
-	}
+     $(".dot2").click(function(){
 
-
-	if(n>=slider.length){
-		n=slider.length;
-	}
-
-	if(n==4){
-		n=3;
-	}
-
-	/*alert(n);*/
-
-	mostrarImagenes(n);
-
-}
-
-function left(){
-
-	/*alert("hi");*/
-
-	slider=document.getElementsByClassName("eventos");
-
-	if(n>0){
-
-		n--;
-
-	}
-
-	if(/*n==0 ||*/ n==(-1)){
-		n=1;
-	}
-
-   /* return n;*/
-
-    /*alert(n);*/
-
-   mostrarImagenes(n);
-}
+    	$(this).siblings().removeClass("active");
+        $(this).addClass("active");
+    	$(".cambiarImg").attr('src',"img/slider/e2.jpg");
+    })
 
 
-window.addEventListener("load" , slider , false);
+    $(".dot3").click(function(){
+
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
+    	$(".cambiarImg").attr('src',"img/slider/e3.jpg");
+    })
 
 
+
+  $(".dot4").click(function(){
+
+         $(this).siblings().removeClass("active");
+         $(this).addClass("active");
+    	$(".cambiarImg").attr('src',"img/slider/e4.jpg");
+    })
+
+
+
+
+})
