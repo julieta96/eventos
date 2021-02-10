93a2b058c3b807ca5f947495e61901373f7e2cfc
julieta96 <julietaextra14@gmail.com>
2021-02-10 12:26:36 -0300

    se modifico el script utilizando event delegation

--- js/slider.js
+++ js/slider.js
@@ -1,37 +1,35 @@
 $(document).ready(function(){
-    
+  
+
+  $( ".dot" ).click(function( event ) {
+
+   let eventoImg = event.target.classList[1] ;
+
+   $(this).siblings().removeClass("active");
+   $(this).addClass("active");
+
+   switch(eventoImg){
+
+    case('dot1'):
+    $(".cambiarImg").attr('src',"img/slider/e1.jpg"); 
+    break;
+
+    case('dot2'):
+    $(".cambiarImg").attr('src',"img/slider/e2.jpg"); 
+    break;
+
+    case('dot3'):
+    $(".cambiarImg").attr('src',"img/slider/e3.jpg"); 
+    break;
+
+    case('dot4'):
+    $(".cambiarImg").attr('src',"img/slider/e4.jpg"); 
+    break;
+
+   }
 
 
-    $(".dot1").on('click',function(){
-       
-          $(this).siblings().removeClass("active");
-          $(this).addClass("active");
-          $(".cambiarImg").attr('src',"img/slider/e1.jpg"); 
-     })
-
-     $(".dot2").click(function(){
-
-    	$(this).siblings().removeClass("active");
-        $(this).addClass("active");
-    	$(".cambiarImg").attr('src',"img/slider/e2.jpg");
-    })
-
-
-    $(".dot3").click(function(){
-
-        $(this).siblings().removeClass("active");
-        $(this).addClass("active");
-    	$(".cambiarImg").attr('src',"img/slider/e3.jpg");
-    })
-
-
-
-  $(".dot4").click(function(){
-
-         $(this).siblings().removeClass("active");
-         $(this).addClass("active");
-    	$(".cambiarImg").attr('src',"img/slider/e4.jpg");
-    })
+  })
 
 
 
