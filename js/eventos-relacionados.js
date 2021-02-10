f13b0965e95023a8faa1c0eaef663354747b654f
julieta96 <julietaextra14@gmail.com>
2021-02-10 17:18:54 -0300

    se agrego modificacion para la clase cambio-pagina

--- js/eventos-relacionados.js
+++ js/eventos-relacionados.js
@@ -20,22 +20,28 @@ $(document).ready(function(){
 
 	})
 
-	$(".a_eventos1").click(function(){
+	$(".detalle").click(function(e){
 
-        /*$(".e-img").attr("src" ,"img/eventos/1.png");*/
-        $("#recitales").css("display","block");
+
+
+       let cambiarEvento = e.target.classList[1] ;
+
+		switch(cambiarEvento){
+
+
+		case('a_eventos1'):
+		$("#recitales").css("display","block");
         $(".re").css("display","block");
         $("#recitales").siblings().css("display","none");
         $(".re").siblings().css("display","none");
 		$(".titulo").text("RECITALES");
 		$(".cambio-hora").text("28/01/2021 20hs");
 		$(".cambio-organizador").text("Iure");
+		$(".cambio-pagina").attr("href","formulario.html#recital");
+		
+        break;
 
-	})
-
-	$(".a_eventos2").click(function(){
-
-        /*$(".e-img").attr("src" ,"img/eventos/2.jpg");*/
+        case('a_eventos2'):
         $("#karaoke").css("display","block");
         $(".ka").css("display","block");
         $(".ka").siblings().css("display","none");
@@ -43,66 +49,64 @@ $(document).ready(function(){
 		$(".titulo").text("KARAOKE");
 		$(".cambio-hora").text("02/09/2021 23hs");
 		$(".cambio-organizador").text("Organizador2");
-
+		$(".cambio-pagina").attr("href","formulario.html#karaoke");
 		
-	})
+		break;
 
-	$(".a_eventos3").click(function(){
-
-        /*$(".e-img").attr("src" ,"img/eventos/3.jpg");*/
-        $("#cumpleanos").css("display","block");
+		case('a_eventos3'):
+		$("#cumpleanos").css("display","block");
         $(".cu").css("display","block");
         $(".cu").siblings().css("display","none");
         $("#cumpleanos").siblings().css("display","none");
 		$(".titulo").text("CUMPLEAÑOS");
 		$(".cambio-hora").text("10/07/2021 20hs");
 		$(".cambio-organizador").text("Organizador3");
-
+		$(".cambio-pagina").attr("href","formulario.html#cumple");
 		
-	})
+		break;
 
-	$(".a_eventos4").click(function(){
-
-        //$(".e-img").attr("src" ,"img/eventos/4.jpg");
-        $("#casamientos").css("display","block");
+		case('a_eventos4'):
+		$("#casamientos").css("display","block");
         $(".ca").css("display","block");
         $(".ca").siblings().css("display","none");
         $("#casamientos").siblings().css("display","none");
 		$(".titulo").text("CASAMIENTOS");
 		$(".cambio-hora").text("20/09/2021 23hs");
 		$(".cambio-organizador").text("Organizador4");
-
+		$(".cambio-pagina").attr("href","formulario.html#casamiento");
 		
-	})
+		break;
 
-	$(".a_eventos5").click(function(){
-
-        //$(".e-img").attr("src" ,"img/eventos/5.jpg");
-        $("#egresados").css("display","block");
+		case('a_eventos5'):
+		$("#egresados").css("display","block");
         $(".eg").css("display","block");
         $(".eg").siblings().css("display","none");
         $("#egresados").siblings().css("display","none");
 		$(".titulo").text("EGRESADOS");
 		$(".cambio-hora").text("12/12/2021 20hs");
 		$(".cambio-organizador").text("Organizador5");
-
+		$(".cambio-pagina").attr("href","formulario.html#egresado");
 		
-	})
+		break;
 
-	$(".a_eventos6").click(function(){
-
-        //$(".e-img").attr("src" ,"img/eventos/6.jpg");
-        $("#fiesta15").css("display","block");
+		case('a_eventos6'):
+		$("#fiesta15").css("display","block");
         $(".fi").css("display","block");
         $(".fi").siblings().css("display","none");
         $("#fiesta15").siblings().css("display","none");
 		$(".titulo").text("FIESTA DE 15");
 		$(".cambio-hora").text("08/08/2021 21hs");
 		$(".cambio-organizador").text("Organizador6");
-
+		$(".cambio-pagina").attr("href","formulario.html#fiesta15");
 		
-	})
 
+		break;
+
+
+
+		}
+	
+	})
 
 
 })
