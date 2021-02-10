dd586215fb5f0374845ece5f56b84a508e17a84f
julieta96 <julietaextra14@gmail.com>
2021-02-10 17:05:17 -0300

    se agrego validacion para pagina formulario

--- js/redireccionarPagina.js
+++ js/redireccionarPagina.js
@@ -8,11 +8,15 @@ $(document).ready(function(){
 		return rutaRelativa;  
 	}   
 
+
+
+	    //para pagina evento--------------------------------------------------------------------------------
+
     	switch(pageHref()){
 
 		case "evento.html#recitales":
 
-		alert(pageHref());
+		//alert(pageHref());
 
 		$("#recitales").css("display","block");
         $(".re").css("display","block");
@@ -26,8 +30,7 @@ $(document).ready(function(){
 
 		case "evento.html#karaoke":
 
-
-		alert(pageHref());
+		/*alert(pageHref());*/
 
 		$("#karaoke").css("display","block");
         $(".ka").css("display","block");
@@ -41,7 +44,7 @@ $(document).ready(function(){
 
 		case "evento.html#cumpleanos":
 
-		alert(pageHref());
+		//alert(pageHref());
 
 		$("#cumpleanos").css("display","block");
         $(".cu").css("display","block");
@@ -54,7 +57,7 @@ $(document).ready(function(){
 		break;
 
 		case "evento.html#casamientos":
-		alert(pageHref());
+		//alert(pageHref());
 		$("#casamientos").css("display","block");
         $(".ca").css("display","block");
         $(".ca").siblings().css("display","none");
@@ -67,7 +70,7 @@ $(document).ready(function(){
 		break;
 
 		case "evento.html#egresados":
-		alert(pageHref());
+		//alert(pageHref());
 		$("#egresados").css("display","block");
         $(".eg").css("display","block");
         $(".eg").siblings().css("display","none");
@@ -79,7 +82,7 @@ $(document).ready(function(){
 		break;
 
 		case "evento.html#fiesta15":
-		alert(pageHref());
+		//alert(pageHref());
 		$("#fiesta15").css("display","block");
         $(".fi").css("display","block");
         $(".fi").siblings().css("display","none");
@@ -87,6 +90,72 @@ $(document).ready(function(){
 		$(".titulo").text("FIESTA DE 15");
 		$(".cambio-hora").text("08/08/2021 21hs");
 		$(".cambio-organizador").text("Organizador6");
+		
+		break;
+
+
+
+	}
+
+
+	//para pagina formulario--------------------------------------------------------------------------------
+
+	switch(pageHref()){
+
+		case "formulario.html#recital":
+
+		//alert(pageHref());
+		$(".titulo-evento").attr("id","recital");
+        $(location).attr("href","formulario.html#recital");
+        $(".titulo-evento").text("RECITAL");
+		break;
+
+		case "formulario.html#karaoke":
+
+		/*alert(pageHref());*/
+
+		$(".titulo-evento").attr("id","karaoke");
+        $(location).attr("href","formulario.html#karaoke");
+        $(".titulo-evento").text("KARAOKE");
+		
+		break;
+
+		case "formulario.html#cumple":
+
+		//alert(pageHref());
+		$(".titulo-evento").attr("id","cumple");
+        $(location).attr("href","formulario.html#cumple");
+        $(".titulo-evento").text("CUMPLEAÑOS");
+		break;
+
+		case "formulario.html#casamiento":
+		//alert(pageHref());
+
+		$(".titulo-evento").attr("id","casamiento");
+        $(location).attr("href","formulario.html#casamiento");
+        $(".titulo-evento").text("CASAMIENTOS");
+		
+
+		
+		break;
+
+		case "formulario.html#egresado":
+		//alert(pageHref());
+
+		$(".titulo-evento").attr("id","egresado");
+        $(location).attr("href","formulario.html#egresado");
+        $(".titulo-evento").text("EGRESADOS");
+		
+		
+		break;
+
+		case "formulario.html#fiesta15":
+		//alert(pageHref());
+
+		$(".titulo-evento").attr("id","fiesta15");
+        $(location).attr("href","formulario.html#fiesta15");
+        $(".titulo-evento").text("FIESTA DE 15");
+		
 		
 		break;
 
