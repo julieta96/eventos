ffb6c85991fb8db502c25a9f28060ba1b1cdc30b
julieta96 <julietaextra14@gmail.com>
2021-02-17 21:37:25 -0300

    validacion modal

---
+++ js/modal.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+function modal(){
+
+let boton= $(".boton2");
+boton.click(function(){
+
+	abrirModal();
+});
+
+let cerrar = $(".close");
+cerrar.click(function(){
+
+	cerrarModal();
+});
+
+
+}
+
+
+function abrirModal(){
+
+let modal=$("#modal");
+let container = $(".modal-content");
+    
+   modal.css("display","block");
+   container.css("transform","translateY(0%)");  
+
+}
+
+
+function cerrarModal(){
+
+ let modal=$("#modal");
+
+ modal.css("display","none");
+
+    
+}
+
+modal();
+
+
+
+
+   })
+
+
