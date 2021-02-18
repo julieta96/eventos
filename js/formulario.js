2cd026b60d59073c0eb0501c576c0e424b66af9c
julieta96 <julietaextra14@gmail.com>
2021-02-17 21:38:03 -0300

    se cambio hide() por remove()

--- js/formulario.js
+++ js/formulario.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
    $(".formulario").on('click', '.remove' , function(){
        
-       $(this).parent().hide();
+       $(this).parent().remove();
 
    })
 
@@ -23,21 +23,22 @@ $(document).ready(function(){
          const input3 = $("<input>");
 
          div.addClass("remove-container");
+         div.addClass("datos");
          input1.addClass("inscripcion");
          input2.addClass("inscripcion");
          input3.addClass("inscripcion");
 
          input1.attr("type" , "text");
-         input1.attr("name" , "");
+         input1.attr("id" , "nombre");
          input1.attr("placeholder" , "Nombre");
 
          input2.attr("type" , "text");
-         input2.attr("name" , "");
+         input2.attr("id" , "apellido");
          input2.attr("placeholder" , "Apellido");
 
 
          input3.attr("type" , "text");
-         input3.attr("name" , "");
+         input3.attr("id" , "telefono");
          input3.attr("placeholder" , "Telefono");
 
 
@@ -48,8 +49,53 @@ $(document).ready(function(){
          div.append(input3);
     	
     })
-     
+    
+    //Validacion de los campos 
 
+
+   // $(".formulario").on('click', '#inscribirse' ,function(){
+
+    /*let inputs=$(".inscripcion");
+    let nombre = $("#nombre");
+    let apellido = $("#apellido");
+    let telefono = $("#telefono");
+    let letras = /^[A-Za-z]+$/;
+    let numeros = /[0-9]/;*/
+
+    //e.preventDefault();
+
+    /*if(!letras.test(nombre.val())){
+
+
+        alert("El nombre debe tener solo letras");
+    }
+
+
+    if(!letras.test(apellido.val())){
+
+
+        alert("El apellido debe tener solo letras");
+    }
+
+
+    if(!numeros.test(telefono.val())){
+
+
+        alert("El telefono debe tener solo numeros");
+    }
+
+
+    if(inputs.length == ""){
+
+        alert("Los campos no pueden estar vacios");
+        inputs.attr("required","");
+    }*/
+
+
+    //})
+
+    
+   
 
 
 })
