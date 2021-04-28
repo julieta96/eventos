3bdefd73fb32981d4292e91bd24f5ad314b4c182
julieta96 <julietaextra14@gmail.com>
2021-04-28 20:45:16 -0300

    validacion para pagina invitacion

---
+++ js/invitacion.js
@@ -0,0 +1,11 @@
+require(['redireccionarPagina','tarjeta-personalizada','tarjeta-personalizada2','personalizar-invitacion','carrito','validacionCarrito'],
+
+	function(r,t,t2,i,carro) {
+
+		r;
+		t;
+		t2;
+		i;
+		carro;
+		
+	})
