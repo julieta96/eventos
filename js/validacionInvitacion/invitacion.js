f827a8c8498f23775d28b5ef53605594d7b0cda2
julieta96 <julietaextra14@gmail.com>
2021-04-29 19:00:00 -0300

    validacion invitacion

---
+++ js/validacionInvitacion/invitacion.js
@@ -0,0 +1,15 @@
+require(['jquery','redireccionarPagina','validacionInvitacion/tarjeta-personalizada','validacionInvitacion/tarjeta-personalizada2','personalizar-invitacion','clases/carrito','clases/instancias/validacionCarrito'],
+
+	function($,r,t,t2,i,carro) {
+
+		r;
+		t;
+		t2;
+		i;
+		carro;
+
+	 $(".marca").click(function(){
+     $(location).attr("href","index.html");
+     })
+		
+	})
