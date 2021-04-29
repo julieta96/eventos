3dd52673797a01e342e40536f91569241fa8f962
julieta96 <julietaextra14@gmail.com>
2021-04-29 18:58:15 -0300

    validacion evento

---
+++ js/validacionEvento/eventos-detalle.js
@@ -0,0 +1,112 @@
+require(['jquery','clases/instancias/eventos','clases/instancias/organizadores'],function($,objeto,org){
+
+	$("#f-izq").click(function(){
+
+		let slider= $(".div-contenedor-img");
+
+		$(slider[0]).css("display","none");
+		$(slider[1]).css("display","block");
+
+	})
+
+
+	$("#f-der").click(function(){
+
+		let slider= $(".div-contenedor-img");
+
+		$(slider[1]).css("display","none");
+		$(slider[0]).css("display","block");
+
+	})
+
+		$(".detalle").click(function(e){
+
+
+
+       let cambiarEvento = e.target.classList[1] ;
+
+		switch(cambiarEvento){
+
+
+		case('a_eventos1'):
+		$("#recitales").css("display","block");
+        $(".re").css("display","block");
+        $("#recitales").siblings().css("display","none");
+        $(".re").siblings().css("display","none");
+		$(".titulo").text(`${objeto.recitalDatos.getNombre}`);
+		$(".cambio-hora").text(`${objeto.recitalDatos.getFecha} ${objeto.recitalDatos.getHora}`);
+		$(".cambio-organizador").text(`${org.luz.getNombre} ${org.luz.getApellido}`);
+		$(".cambio-pagina").attr("href","formulario.html#recital");
+		
+        break;
+
+        case('a_eventos2'):
+        $("#karaoke").css("display","block");
+        $(".ka").css("display","block");
+        $(".ka").siblings().css("display","none");
+        $("#karaoke").siblings().css("display","none");
+		$(".titulo").text(`${objeto.karaokeDatos.getNombre}`);
+		$(".cambio-hora").text(`${objeto.karaokeDatos.getFecha} ${objeto.karaokeDatos.getHora}`);
+		$(".cambio-organizador").text(`${org.janet.getNombre} ${org.janet.getApellido}`);
+		$(".cambio-pagina").attr("href","formulario.html#karaoke");
+		
+		break;
+
+		case('a_eventos3'):
+		$("#cumpleanos").css("display","block");
+        $(".cu").css("display","block");
+        $(".cu").siblings().css("display","none");
+        $("#cumpleanos").siblings().css("display","none");
+		$(".titulo").text(`${objeto.cumpleDatos.getNombre}`);
+		$(".cambio-hora").text(`${objeto.cumpleDatos.getFecha} ${objeto.cumpleDatos.getHora}`);
+		$(".cambio-organizador").text(`${org.camilo.getNombre} ${org.camilo.getApellido}`);
+		$(".cambio-pagina").attr("href","formulario.html#cumple");
+		
+		break;
+
+		case('a_eventos4'):
+		$("#casamientos").css("display","block");
+        $(".ca").css("display","block");
+        $(".ca").siblings().css("display","none");
+        $("#casamientos").siblings().css("display","none");
+		$(".titulo").text(`${objeto.casamientoDatos.getNombre}`);
+		$(".cambio-hora").text(`${objeto.casamientoDatos.getFecha} ${objeto.casamientoDatos.getHora}`);
+		$(".cambio-organizador").text(`${org.carlos.getNombre} ${org.carlos.getApellido}`);
+		$(".cambio-pagina").attr("href","formulario.html#casamiento");
+		
+		break;
+
+		case('a_eventos5'):
+		$("#egresados").css("display","block");
+        $(".eg").css("display","block");
+        $(".eg").siblings().css("display","none");
+        $("#egresados").siblings().css("display","none");
+		$(".titulo").text(`${objeto.egresadoDatos.getNombre}`);
+		$(".cambio-hora").text(`${objeto.egresadoDatos.getFecha} ${objeto.egresadoDatos.getHora}`);
+		$(".cambio-organizador").text(`${org.luis.getNombre} ${org.luis.getApellido}`);
+		$(".cambio-pagina").attr("href","formulario.html#egresado");
+		
+		break;
+
+		case('a_eventos6'):
+		$("#fiesta15").css("display","block");
+        $(".fi").css("display","block");
+        $(".fi").siblings().css("display","none");
+        $("#fiesta15").siblings().css("display","none");
+		$(".titulo").text(`${objeto.fiesta15Datos.getNombre}`);
+		$(".cambio-hora").text(`${objeto.fiesta15Datos.getFecha} ${objeto.fiesta15Datos.getHora}`);
+		$(".cambio-organizador").text(`${org.paola.getNombre} ${org.paola.getApellido}`);
+		$(".cambio-pagina").attr("href","formulario.html#fiesta15");
+		
+
+		break;
+
+
+
+		}
+	
+	})
+
+
+
+})
---
+++ js/validacionEvento/eventos.js
@@ -0,0 +1,9 @@
+require(['jquery', 'validacionEvento/eventos-detalle','redireccionarPagina','clases/carrito','clases/instancias/validacionCarrito'], function($,er,r,carro){
+
+	er;
+	r;
+	carro;
+	$(".marca").click(function(){
+     $(location).attr("href","index.html");
+     })
+})
