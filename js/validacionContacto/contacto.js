c5d9f071dab83f47a99c1a3258b7fe46b8c20b2d
julieta96 <julietaextra14@gmail.com>
2021-04-29 19:14:50 -0300

    validacion pagina contacto

---
+++ js/validacionContacto/contacto.js
@@ -0,0 +1,7 @@
+require(['jquery','validacionContacto/validacionFormularioContacto','clases/carrito','clases/instancias/validacionCarrito'],function($,vfc){
+
+	$(".marca").click(function(){
+     $(location).attr("href","index.html");
+     })
+
+})
