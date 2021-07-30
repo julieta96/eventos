7ebc672e439ebcb9d0ecf9ce736b8e409667f308
julieta96 <julietaextra14@gmail.com>
2021-07-30 12:11:42 -0300

    print invitacion

--- js/validacionInvitacion/invitacion.js
+++ js/validacionInvitacion/invitacion.js
@@ -11,5 +11,28 @@ require(['jquery','redireccionarPagina','validacionInvitacion/tarjeta-personaliz
 	 $(".marca").click(function(){
      $(location).attr("href","index.html");
      })
+
+     //let id = $(".tarjeta").attr("id");
+
+      let idTarjeta = $(".tarjeta").attr("id");
+
+      
+
+     $(".imprimir").click(function(){
+       
+       console.log(idTarjeta);
+
+     	let imprimirTarjeta = document.getElementById(idTarjeta).innerHTML;
+     	let w = window.open();
+     	w.document.write(imprimirTarjeta);
+     	w.document.close();
+     	w.focus(); 
+		w.print();
+		w.close()
+		return true;
+
+     	
+     })
+
 		
 	})
