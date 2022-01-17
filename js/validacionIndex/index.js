33ef59246271f6cb917ee3a3f2c19d5b234a715d
julieta96 <julietaextra14@gmail.com>
2022-01-17 20:58:18 -0300

    se modifico el llamado de archivos js

--- js/validacionIndex/index.js
+++ js/validacionIndex/index.js
@@ -1,11 +1,12 @@
-require(['jquery','backIndex','clases/instancias/eventos' ,'validacionIndex/slider','validacionIndex/slider-automatico','validacionIndex/validacion_js','clases/carrito','clases/instancias/validacionCarrito','events-search','menu_responsive'],
+require(['jquery','backIndex','clases/instancias/eventos' ,'validacionIndex/slider','validacionIndex/slider-automatico','validacionIndex/pasos-a-comprar-evento','clases/carrito','clases/instancias/validacionCarrito','events-search','menu_responsive'],
 
- ($,b,objetos ,slider,sliderA,pasos,c,carro, search, mr)=>{
+ ($,backIndex,objetos ,slider,sliderA,pasosEvento,carroClase,carroObjeto, search, menuResponsive)=>{
 
+     backIndex;
      objetos.eventosDatos;
      slider;
      sliderA;
-     pasos;
+     pasosEvento;
 
      $(".e1").attr("id",objetos.recitalDatos.getId);
      $(".e2").attr("id",objetos.karaokeDatos.getId);
@@ -20,10 +21,11 @@ require(['jquery','backIndex','clases/instancias/eventos' ,'validacionIndex/slid
      $(".e4 > .casamiento").attr("id",objetos.casamientoDatos.getId);
      $(".e5 > .egresados").attr("id",objetos.egresadoDatos.getId);
      $(".e6 > .fiesta15").attr("id",objetos.fiesta15Datos.getId);
-     c;
-     carro;
+     
+     carroClase;
+     carroObjeto;
      search;
-     mr;
+     menuResponsive;
 })
 
 
