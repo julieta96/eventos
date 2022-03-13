d08fc3c1d5109ebb7cad575e8ce3c0a905d5a603
julieta96 <julietaextra14@gmail.com>
2022-03-13 20:14:43 -0300

    maxlength

--- js/validacionContacto/validacionFormularioContacto.js
+++ js/validacionContacto/validacionFormularioContacto.js
@@ -55,17 +55,18 @@ require(['jquery','clases/testCharacter'], ($,t)=>{
 
         		let size = ($("#comentarios").val()).length;
 
-        			if(size<230){
+        			if(size<$("#comentarios").attr("maxlength")){
 
         				$("#comentarios").css("background","#4CAF50");
                         $("#texto-alert").css("display","none");
-                        console.log(size);
+                        console.log(size)
+                        
 
         			}else{
-                        $("#comentarios").attr("maxlength","100");
         				$("#comentarios").css("background","#FF5252");
                         $("#texto-alert").css("display","block");
-                        console.log(size);
+                        console.log(size)
+                        
 
         			}
 
