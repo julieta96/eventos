4ee10db2555d73bb7e4a3f84999bbf453bfdbf2d
julieta96 <julietaextra14@gmail.com>
2022-03-20 20:29:46 -0300

    update pass

--- js/pass.js
+++ js/pass.js
@@ -3,11 +3,11 @@ define([],() =>{
 	return{
 		eventos:'NDU4b3B5dXVXREE=',
 		recital:'MTAwV1hZejAw',
-		karaoke:'',
-		cumple:'',
-		casamiento:'',
-		egresado:'',
-		fiesta15:''
+		karaoke:'TlBISDc1NjA4Nzc=',
+		cumple:'NDU0NTVURkdR',
+		casamiento:'NTQ2NzVZWVJUWQ==',
+		egresado:'MkdISDU0UkU=',
+		fiesta15:'DQpTREZ3ZXIzNA=='
 	}
 
 
