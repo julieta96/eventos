7c2d90252658747a376c97228bc6504b96c76b9c
julieta96 <julietaextra14@gmail.com>
2022-03-21 20:14:20 -0300

    ahora solo se puede personalizar evento comprado

--- js/clases/instancias/validacionCarrito.js
+++ js/clases/instancias/validacionCarrito.js
@@ -4,7 +4,7 @@ require(['jquery','clases/carrito'],
 		const carrito = new c(1);
 
 		//let eventoComprado=null;
-        let contador = localStorage.getItem("cantidadEventos") || 0; 
+        let contador = localStorage.getItem("cantidadEventos") || localStorage.setItem("cantidadEventos", 0); 
 		let contador10 = localStorage.getItem("contador-re")|| localStorage.setItem("contador-re", 0);
 		let contador20 = localStorage.getItem("contador-kk")|| localStorage.setItem("contador-kk", 0);
 		let contador30 = localStorage.getItem("contador-cm")|| localStorage.setItem("contador-cm", 0);
@@ -26,24 +26,28 @@ require(['jquery','clases/carrito'],
 				contador10++
 				carrito.agregarCantidadEvento(eventoComprado ,contador10);
 				localStorage.setItem("contador-re",contador10);
+				localStorage.setItem(`${contadorEvento}Comprado`,contadorEvento);
 				break;
 
 				case "karaoke":
 				contador20++
 				carrito.agregarCantidadEvento(eventoComprado ,contador20);
 				localStorage.setItem("contador-kk",contador20);
+				localStorage.setItem(`${contadorEvento}Comprado`,contadorEvento);
 				break;
 
 				case "cumple":
 				contador30++
 				carrito.agregarCantidadEvento(eventoComprado ,contador30);
 				localStorage.setItem("contador-cm",contador30);
+				localStorage.setItem(`${contadorEvento}Comprado`,contadorEvento);
 				break;
 
 				case "casamiento":
 				contador40++
 				carrito.agregarCantidadEvento(eventoComprado ,contador40);
 				localStorage.setItem("contador-ca",contador40);
+				localStorage.setItem(`${contadorEvento}Comprado`,contadorEvento);
 
 				break;
 
@@ -51,14 +55,14 @@ require(['jquery','clases/carrito'],
 				contador50++
 				carrito.agregarCantidadEvento(eventoComprado ,contador50);
 				localStorage.setItem("contador-eg",contador50);
-
+				localStorage.setItem(`${contadorEvento}Comprado`,contadorEvento);
 				break;
 
 				case "fiesta15":
 				contador60++
 				carrito.agregarCantidadEvento(eventoComprado ,contador60);
 				localStorage.setItem("contador-15",contador60);
-
+				localStorage.setItem(`${contadorEvento}Comprado`,contadorEvento);
 				break;
 			}
 
--- js/validacionEntradas/personalizarTarjeta.js
+++ js/validacionEntradas/personalizarTarjeta.js
@@ -1,48 +1,17 @@
 require(['jquery'] , ($) =>{
 
-	let carritoCargado = localStorage.getItem("compra");
-	let carro=JSON.parse(carritoCargado);
-
 	let personalizar = $(".personalizar");
-	let modificarPersonalizar = $(".m-personalizar");
-	let array = [];
 
 	$(personalizar).each((i)=>{
 
-		let id=$(personalizar[i]).attr('id');
+			let eventoComprado= $(personalizar[i]).attr('id');
+			let buscarEventoComprado = localStorage.getItem(`${eventoComprado}Comprado`);
 
-		let evento = id.substring(0, id.length);
+			if(buscarEventoComprado==eventoComprado){
 
-		array.push({id: `${evento}`});
-	})
-
-	/*$(modificarPersonalizar).each((i)=>{
-
-		$(modificarPersonalizar[i]).removeAttr('disabled').addClass('comprado');
-	})*/
-
-	let id="";
-
-	$(carro).each((i)=>{
-
-		$(personalizar).each((j)=>{
-
-			let nombre = carro[i].nombre.toLowerCase();
-
-			if(nombre.includes(array[j].id)){
-
-				id = array[j].id;
-				$(`#${id}`).removeAttr('disabled');
-				$(`#${id}`).addClass('comprado');
+				$(personalizar[i]).removeAttr('disabled');
 			}
 
-		})
-
 	})
-
-	
-
-	
-
 
 });
